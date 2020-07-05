f4e951338895553621ae9cd261cdf67ad6d81246
siddy <saidimmaka@gmail.com>
2020-07-05 15:37:51 +0300

    Implemented second route

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,34 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void> (
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asCamelCase,
+                    style: _biggerFont,
+                  ),
+                );
+              }
+          );
 
+          final divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided,),
+          );
+        },
+      )
+    );
   }
 }
 
